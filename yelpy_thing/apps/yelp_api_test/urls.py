1ebeb09f940383489683ac8b142353cbfc7afb30
JKobuchi <jekobuchi@gmail.com>
2017-08-22 11:18:46 -0700

    django_project

---
+++ yelpy_thing/apps/yelp_api_test/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.index),
+    url(r'^home$', views.home),
+    url(r'^process$', views.process),
+    url(r'^results$', views.results),
+    url(r'^add_friend$', views.add_friend),
+    url(r'^goback$', views.goback),
+]
---
+++ yelpy_thing/apps/yelp_api_test/views.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.shortcuts import render, redirect, HttpResponse
+
+from fusion import *
+# Create your views here.
+
+def index(request):
+    if 'id' in request.session:
+        request.session['id'] = None
+    return render(request, 'yelp_api_test/index.html')  
+
+def login(request):
+    pass
+
+def register(request):
+    pass
+
+def logout(request):
+    return redirect('/')  
+
+def home(request):
+    return render(request, 'yelp_api_test/home.html')  
+
+def add_friend(request):
+    
+    return redirect('/home')  
+
+def process(request):
+    if request.method == 'POST':
+        request.session['search'] = request.POST['search']
+        request.session['search_results'] = query_api(request.POST['search'], 'san jose')
+        return redirect('/results')
+    else:
+        return redirect('/')
+
+def results(request):
+    search_results = request.session['search_results']
+    context = {
+        'search_results': search_results
+    }
+    return render(request, 'yelp_api_test/results.html', context)
+
+def goback(request):
+    return redirect('/home')
